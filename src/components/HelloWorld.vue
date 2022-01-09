10036676db9356821d80b04f9047ce64a244b79a
kevas007 <kevassiobo@gmail.com>
2022-01-09 15:07:44 +0100

    base

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="m-10" v-if="getTodos  ">
+ <p>{{getTodos.length}}</p>
+ <div class="grid grid-cols-4 gap-4">
+      <div v-for="item in getTodos" :key="item.id" class="max-w-md px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <div  class="flex justify-center -mt-16 md:justify-end" v-if="item.image_cover">
+            <img   class="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" :src="item.image_cover"/>
+        </div>
+
+        <h2 class="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0 md:text-3xl">{{item.title }}</h2>
+
+        <p class="mt-2 text-gray-600 dark:text-gray-200">{{item.description }}</p>
+         <div class="flex justify-end mt-4 mb-5">
+         <input type="checkbox" v-model="item.completed" id="horns" name="horns">
+         </div>
+        <hr>
+        <div class="flex justify-around mt-4">
+          <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+            Edit
+</button>
+
+          <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80" >
+      Delete
+            </button>
+
+          </div>
+        <!-- <div class="flex justify-end mt-4">
+            <a href="#" class="text-xl font-medium text-blue-500 dark:text-blue-300">John Doe</a>
+        </div> -->
+    </div>
+ </div>
   </div>
 </template>
 
 <script>
+ import { mapGetters } from 'vuex';
+
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+  mounted () {
+    this.$store.dispatch('getTodos')
+  },
+  computed: {
+     ...mapGetters([
+      'getTodos',
+    ])
+
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: null,
   },
   mutations: {
+    setTodos(state, todos) {
+      state.todos = todos
+    },
   },
   actions: {
+    getTodos({ commit }) {
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/todos',
+      })
+        .then(function (response) {
+          commit('setTodos', response.data.data.todos)
+
+          // console.log(response.data.data.todos)
+        })
+    },
+  },
+  getters: {
+    getTodos: state => state.todos,
   },
   modules: {
   }
